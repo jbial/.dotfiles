f5d28d74417d9a69cb6b1a1cba6dc830b77cdc35
Jeffrey Lai <jlai@utexas.edu>
2025-09-07 20:53:49 -0500

    nit nvim config

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -15,6 +15,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  lockfile = vim.fn.stdpath("data") .. "/lazy/lazy-lock.json",
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
